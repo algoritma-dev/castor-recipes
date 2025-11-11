30bb4751e9713eac15ec17a80258780d2da872ec
Raffaele Carelle <raffaele.carelle@algoritma.it>
2025-11-11 15:51:19 +0100

    Improve Xdebug tasks with better environment handling and error feedback
    - Add `DOCKER_SERVICE` environment variable management for enabling/disabling Xdebug.
    - Improve error handling to detect and warn if Xdebug is already enabled/disabled.
    - Add informative success messages for task completion.
    - Ensure proper restoration of environment variables after task execution.

--- recipes/_xdebug.php
+++ recipes/_xdebug.php
@@ -3,32 +3,50 @@
 use Castor\Attribute\AsTask;
 
 use function Castor\run;
+use function Castor\io;
 
 #[AsTask(name: 'enable', namespace: 'xdebug', description: 'Enable Xdebug')]
 function xdebug_enable(string $fpmServiceName = 'php-fpm', string $webServerServiceName = 'webserver'): void
 {
-    $hasXdebug = \Castor\capture(dockerize('php -m | grep xdebug'));
+    set_env('DOCKER_SERVICE', $fpmServiceName);
     $iniPath = '/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini';
 
-    if ($hasXdebug === '' || $hasXdebug === '0') {
+    try{
         run(dockerize(sprintf('sudo mv %s.disabled %s', $iniPath, $iniPath)));
+    } catch (\Throwable $e) {
+        io()->warning('Xdebug is already disabled');
+        return;
+    }
 
+    if(env_value('CASTOR_DOCKER') === 'true' || env_value('CASTOR_DOCKER') !== '1') {
         docker_compose_restart($fpmServiceName);
         docker_compose_restart($webServerServiceName);
     }
+
+    restore_env('DOCKER_SERVICE');
+
+    io()->success('Xdebug is enabled. Run `castor xdebug:disable` to disable it again.');
 }
 
 #[AsTask(name: 'disable', namespace: 'xdebug', description: 'Disable Xdebug')]
 function xdebug_disable(string $fpmServiceName = 'php-fpm', string $webServerServiceName = 'webserver'): void
 {
-    $hasXdebug = \Castor\capture(dockerize('php -m | grep xdebug'));
-
+    set_env('DOCKER_SERVICE', $fpmServiceName);
     $iniPath = '/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini';
 
-    if ($hasXdebug !== '' && $hasXdebug !== '0') {
+    try {
         run(dockerize(sprintf('sudo mv %s %s.disabled', $iniPath, $iniPath)));
+    } catch (\Throwable $e) {
+        io()->warning('Xdebug is already disabled');
+        return;
+    }
 
+    if(env_value('CASTOR_DOCKER') === 'true' || env_value('CASTOR_DOCKER') !== '1') {
         docker_compose_restart($fpmServiceName);
         docker_compose_restart($webServerServiceName);
     }
+
+    restore_env('DOCKER_SERVICE');
+
+    io()->success('Xdebug is disabled. Run `castor xdebug:enable` to enable it again.');
 }
